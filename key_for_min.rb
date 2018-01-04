d3c5dd1cb6612fcf59b6ff8097e5f5e44dd455d5
tal shachar <tal.shachar@buzzfeed.com>
2018-01-04 05:25:51 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,18 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  values = []
+  name_hash.collect do |key, value|
+      values.push(value)
+  end
+values.sort do |a, b|
+ if a == b
+   0
+ elsif a < b
+   -1
+ elsif a > b
+   1
+ end
+end
+name_hash[value[0]]
 end
